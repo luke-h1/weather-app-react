5bcf6f72393dea55e7ad26ba4d115e9326f79daf
luke-h1 <luke.howsam@yahoo.com>
2021-02-28 13:01:22 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Deploying API to heroku 🤠"
+cd backend 
+git add * 
+git commit -am "deploy"
+git push heroku main
+echo "deployed backend to heroku"
+echo ""
+echo ""
+echo "deploying frontend to vercel 🤠"
+cd ../frontend && vc && vc --prod 
