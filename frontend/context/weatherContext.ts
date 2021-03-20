b71fa058594981565d1aa9595782dde412e71517
luke-h1 <luke.howsam@yahoo.com>
2021-03-20 11:47:32 +0000

    deploy

--- frontend/context/weatherContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-
-const weatherContext = createContext([]);
-export default weatherContext;
--- frontend/validations/WeatherValidation.tsx
+++
@@ -1,5 +0,0 @@
-import * as yup from 'yup';
-
-export const WeatherSchema = yup.object().shape({
-  city: yup.string().min(3).max(20).required(),
-});
