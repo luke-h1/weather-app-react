2406a3d9a56ae76cc674b44b64c101d4b8a3b868
luke-h1 <luke.howsam@yahoo.com>
2021-03-20 13:07:48 +0000

    update deploy script

---
+++ server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ssh into digital ocean droplet, pull latest changes 
+# and restart pm2 for changes to take affect
+ssh root@ip-address
+cd apps/weather-app-react
+git pull 
+cd server
+pm2 restart dist/src/index.js
