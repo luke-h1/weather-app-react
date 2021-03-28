a53fa6fa30cfcd9d2c5b9defa7184b5c9333859c
luke-h1 <luke.howsam@yahoo.com>
2021-03-28 15:29:37 +0100

    feat/fix-cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,14 +24,6 @@ const main = async () => {
   app.use(express.json());
   app.disable('x-powered-by');
   app.use(cors());
-  app.use((_, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
-  });
   const PORT = process.env.PORT || 5000;
   app.get('/', (_, res) => {
     res.status(200).json({ message: 'API is running 🚀' });
